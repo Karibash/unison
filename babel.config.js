7dcfc1b6f74f91e98dbeef0446102995481aa347
Karibash <y.karibayashi@gmail.com>
2021-10-18 20:02:25 +0900

    Add a project foundation

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { loose: true }],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    ['@babel/plugin-transform-runtime', { version: require('@babel/runtime/package.json').version }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
+};
